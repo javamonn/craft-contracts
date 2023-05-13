9a744c42617614fa4bc22b5c7cfa6c9a5c8e8d10
Daniel Ramirez <javamonn@hey.com>
2023-05-13 11:27:36 -0400

    Fit to square grid

--- src/CraftSettlementRenderer.sol
+++ src/CraftSettlementRenderer.sol
@@ -22,13 +22,13 @@ contract CraftSettlementRenderer is Owned {
         UNKNOWN
     }
 
-    uint8 constant GRID_SIZE = 24;
+    uint8 constant GRID_ROWS = 24;
+    uint8 constant GRID_COLS = 40;
     string constant SVG_STYLES_GRID = string(
         abi.encodePacked(
             "@font-face{font-family:Unifont;src:url('https://raw.githubusercontent.com/fontsource/fontsource/main/fonts/other/unifont/files/unifont-latin-400-normal.woff');}",
-            ".a{width:576px;height:576px;background-color:black;}",
-            ".b{box-sizing:border-box;width:576px;height:576px;padding:24px;font-family:Unifont;font-size:1rem;display:flex;flex-direction:column;justify-content:center;align-items:center;}",
-            "body,svg{overflow-x:hidden;overflow-y:hidden;margin:0;padding:0;}",
+            ".a{width:416px;height:416px;background-color:black;box-sizing:border-box;font-size:16px;font-family:Unifont;display:flex;flex-direction:column;justify-content:center;align-items:center;}",
+            "svg{overflow-x:hidden;overflow-y:hidden;margin:0;padding:0;}",
             ".row{height:1rem;}",
             ".t{width:.6rem;display:inline-block;height:1rem;}",
             ".i{font-style:italic;}"
@@ -49,17 +49,16 @@ contract CraftSettlementRenderer is Owned {
 
     string constant SVG_HEADER = string(
         abi.encodePacked(
-            "<svg version='2.0' encoding='utf-8' viewBox='0 0 576 576' preserveAspecRatio='xMidyMid' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg'>",
+            "<svg version='2.0' encoding='utf-8' viewBox='0 0 416 416' preserveAspectRatio='xMidYMid' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg'>",
             "<style>",
             SVG_STYLES_GRID,
             SVG_STYLES_COLOR,
             "</style>",
-            "<foreignObject x='0' y='0' width='576' height='576'>",
-            "<div class='a' xmlns='http://www.w3.org/1999/xhtml'>",
-            "<div class='b'>"
+            "<foreignObject x='0' y='0' width='416' height='416'>",
+            "<div class='a' xmlns='http://www.w3.org/1999/xhtml'>"
         )
     );
-    string constant SVG_FOOTER = string(abi.encodePacked("</div>", "</div>", "</foreignObject>", "</svg>"));
+    string constant SVG_FOOTER = string(abi.encodePacked("</div>", "</foreignObject>", "</svg>"));
 
     mapping(Terrain => string[]) TERRAIN_CHARS;
 
@@ -80,8 +79,9 @@ contract CraftSettlementRenderer is Owned {
     }
 
     function getSeed(address owner) internal pure returns (bytes memory) {
-        bytes32[9] memory hashes;
-        for (uint8 i; i < 9;) {
+        bytes32[15] memory hashes;
+
+        for (uint8 i; i < 15;) {
             if (i == 0) {
                 hashes[i] = keccak256(abi.encodePacked(owner));
             } else {
@@ -92,19 +92,33 @@ contract CraftSettlementRenderer is Owned {
                 ++i;
             }
         }
-        // 9 x bytes32 = 288 bytes total
-        bytes memory hash = bytes.concat(
-            hashes[0], hashes[1], hashes[2], hashes[3], hashes[4], hashes[5], hashes[6], hashes[7], hashes[8]
-        );
 
-        return hash;
+        bytes memory output;
+        for (uint8 i; i < 3;) {
+            output = bytes.concat(
+                output,
+                hashes[i * 5],
+                hashes[i * 5 + 1],
+                hashes[i * 5 + 2],
+                hashes[i * 5 + 3],
+                hashes[i * 5 + 4]
+            );
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        return output;
     }
 
-    function getTerrains(bytes memory seed) public pure returns (Terrain[576] memory) {
-        // 4 bits per terrain, 288 byte input
-        Terrain[576] memory terrains;
+    function getTerrains(bytes memory seed) public pure returns (Terrain[960] memory) {
+
+        // 4 bits per terrain, 480 byte input
+        Terrain[960] memory terrains;
         uint256 seedLength = seed.length;
         uint16 unknownTerrainCount;
+
         for (uint16 i = 0; i < seedLength;) {
             bytes1 b = seed[i];
 
@@ -177,33 +191,33 @@ contract CraftSettlementRenderer is Owned {
 
             Terrain[8] memory neighbors = [
                 // Top Left
-                idx < (GRID_SIZE + 1) || (idx % GRID_SIZE) == 0
+                idx < (GRID_COLS + 1) || (idx % GRID_COLS) == 0
                     ? Terrain.UNKNOWN
-                    : terrains[idx - (GRID_SIZE + 1)],
+                    : terrains[idx - (GRID_COLS + 1)],
                 // Top
-                idx < GRID_SIZE ? Terrain.UNKNOWN : terrains[idx - GRID_SIZE],
+                idx < GRID_COLS ? Terrain.UNKNOWN : terrains[idx - GRID_COLS],
                 // Top Right
-                idx < (GRID_SIZE - 1) || (idx % GRID_SIZE) == GRID_SIZE - 1
+                idx < (GRID_COLS - 1) || (idx % GRID_COLS) == GRID_COLS - 1
                     ? Terrain.UNKNOWN
-                    : terrains[idx - (GRID_SIZE - 1)],
+                    : terrains[idx - (GRID_COLS - 1)],
                 // Left
-                idx < 1 || (idx % GRID_SIZE) == 0 ? Terrain.UNKNOWN : terrains[idx - 1],
+                idx < 1 || (idx % GRID_COLS) == 0 ? Terrain.UNKNOWN : terrains[idx - 1],
                 // Right
-                idx + 1 > (terrains.length - 1) || (idx % GRID_SIZE) == GRID_SIZE - 1
+                idx + 1 > (terrains.length - 1) || (idx % GRID_COLS) == GRID_COLS - 1
                     ? Terrain.UNKNOWN
                     : terrains[idx + 1],
                 // Bottom Left
-                idx + (GRID_SIZE - 1) > (terrains.length - 1) || (idx % GRID_SIZE) == 0
+                idx + (GRID_COLS - 1) > (terrains.length - 1) || (idx % GRID_COLS) == 0
                     ? Terrain.UNKNOWN
-                    : terrains[idx + (GRID_SIZE - 1)],
+                    : terrains[idx + (GRID_COLS - 1)],
                 // Bottom
-                idx + GRID_SIZE > (terrains.length - 1)
+                idx + GRID_COLS > (terrains.length - 1)
                     ? Terrain.UNKNOWN
-                    : terrains[idx + GRID_SIZE],
+                    : terrains[idx + GRID_COLS],
                 // Bottom Right
-                idx + GRID_SIZE + 1 > (terrains.length - 1) || (idx % GRID_SIZE) == (GRID_SIZE - 1)
+                idx + GRID_COLS + 1 > (terrains.length - 1) || (idx % GRID_COLS) == (GRID_COLS - 1)
                     ? Terrain.UNKNOWN
-                    : terrains[idx + GRID_SIZE + 1]
+                    : terrains[idx + GRID_COLS + 1]
             ];
 
             for (uint8 j = 0; j < 8;) {
@@ -240,8 +254,8 @@ contract CraftSettlementRenderer is Owned {
 
     function getImage(address owner) public view returns (string memory) {
         bytes memory seed = getSeed(owner);
-        Terrain[576] memory terrains = getTerrains(seed);
-        string[576] memory renderedTerrains;
+        Terrain[960] memory terrains = getTerrains(seed);
+        string[960] memory renderedTerrains;
 
         // 1 bit per character, 4 bits per terrain
         uint256 seedLength = seed.length;
@@ -270,39 +284,45 @@ contract CraftSettlementRenderer is Owned {
         }
 
         string memory output = SVG_HEADER;
-        for (uint16 i = 0; i < 24;) {
-            for (uint16 j = 0; j < 4;) {
+        for (uint16 i = 0; i < GRID_ROWS;) {
+            for (uint16 j = 0; j < 5;) {
                 if (j == 0) {
                     output = string.concat(
                         output,
                         "<div class='row'>",
-                        renderedTerrains[i * 24 + j * 6],
-                        renderedTerrains[i * 24 + j * 6 + 1],
-                        renderedTerrains[i * 24 + j * 6 + 2],
-                        renderedTerrains[i * 24 + j * 6 + 3],
-                        renderedTerrains[i * 24 + j * 6 + 4],
-                        renderedTerrains[i * 24 + j * 6 + 5]
+                        renderedTerrains[i * GRID_COLS + j * 8],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 1],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 2],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 3],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 4],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 5],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 6],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 7]
                     );
-                } else if (j == 3) {
+                } else if (j == 4) {
                     output = string.concat(
                         output,
-                        renderedTerrains[i * 24 + j * 6],
-                        renderedTerrains[i * 24 + j * 6 + 1],
-                        renderedTerrains[i * 24 + j * 6 + 2],
-                        renderedTerrains[i * 24 + j * 6 + 3],
-                        renderedTerrains[i * 24 + j * 6 + 4],
-                        renderedTerrains[i * 24 + j * 6 + 5],
+                        renderedTerrains[i * GRID_COLS + j * 8],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 1],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 2],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 3],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 4],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 5],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 6],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 7],
                         "</div>"
                     );
                 } else {
                     output = string.concat(
                         output,
-                        renderedTerrains[i * 24 + j * 6],
-                        renderedTerrains[i * 24 + j * 6 + 1],
-                        renderedTerrains[i * 24 + j * 6 + 2],
-                        renderedTerrains[i * 24 + j * 6 + 3],
-                        renderedTerrains[i * 24 + j * 6 + 4],
-                        renderedTerrains[i * 24 + j * 6 + 5]
+                        renderedTerrains[i * GRID_COLS + j * 8],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 1],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 2],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 3],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 4],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 5],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 6],
+                        renderedTerrains[i * GRID_COLS + j * 8 + 7]
                     );
                 }
 
